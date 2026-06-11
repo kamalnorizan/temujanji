0656a36badee5ba04e9dc5447cbbf020d3d0de1d
Kamal Norizan <kamalnorizan@gmail.com>
2026-06-11 10:05:18 +0800

    Service for Appointment

---
+++ app/Services/AppointmentTimelineService.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Appointment;
+use App\Models\AppointmentTimeline;
+
+class AppointmentTimelineService
+{
+    public function create(Appointment $appointment, string $title, ?string $description = null, ?string $status= null, ?int $userId = null): AppointmentTimeline {
+
+        if(!$userId && auth()->check()) {
+            $userId = auth()->id();
+        }
+
+        $timeline = new AppointmentTimeline();
+        $timeline->appointment_id = $appointment->id;
+        $timeline->title = $title;
+        $timeline->description = $description;
+        $timeline->status = $status;
+        $timeline->user_id = $userId;
+        $timeline->save();
+
+        return $timeline;
+    }
+}
